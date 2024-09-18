54d9762f28d24c8ab0858cf4c3bc4c279df6e517
namuKR <desbey1@gmail.com>
2024-09-18 15:50:08 +0900

    Cease Project

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -8,11 +8,11 @@ function WelcomePage({ onStart }) {
             <div>
                 <Typography level="h1">가장 쉽고 간단한</Typography>
                 <Typography level="h1">건강 상태 진단 앱</Typography>
-                <p>9/18일까지 접속 가능</p>
+                <p>해당 프로젝트는 종료되었습니다.</p>
             </div>
-            <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+            {/* <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
                 <Button onClick={onStart} size='xl' color='success'>시작하기</Button>
-                </div>
+                </div> */}
             
         </div>
     );
